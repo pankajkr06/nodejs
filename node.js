deb829ca9c6e5d6f97cecc39fd3ac6368406160b
pankajkr06 <pankajkr06@gmail.com>
2024-06-13 00:05:14 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,9 +1,8 @@
 // server.js
 const http = require('http')
-const PORT = process.env.PORT || 9000
-http
-  .createServer((req, res) => {
-    console.log('New connection')
-    res.end('Hello Cloud Run')
+const server = http.createServer((req, res) => {
+        console.log('New connection')
+        res.end('Hello Cloud Run')
   })
-  .listen(PORT, () => console.log('Listening on', PORT))
+const PORT = process.env.PORT || 8080
+server.listen(PORT, () => console.log('Listening'))
